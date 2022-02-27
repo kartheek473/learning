d10a47404a23b5dce3794bb765f8d70b3eed3316
kartheek473 <100348749+kartheek473@users.noreply.github.com>
2022-02-27 12:43:55 +0530

    Add files via upload

---
+++ phase1/practice/calDemo.java
@@ -0,0 +1,22 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class calDemo {
+
+	public static void main(String[] args) {
+		int num1, num2;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter 1st number: ");
+		num1 = in.nextInt();
+		
+		System.out.println("Enter 2st number: ");
+		num2 = in.nextInt();
+		
+		System.out.println("Sum = "+(num1+num2));
+		System.out.println("Subtraction = "+(num1-num2));
+		System.out.println("multiplication = "+(num1*num2));
+		System.out.println("division = "+(num1/num2));
+	}
+
+}
