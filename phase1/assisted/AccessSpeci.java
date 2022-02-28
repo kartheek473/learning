e67a2ebe362866015da8e11c0f0f9d9880654d1f
kartheek473 <100348749+kartheek473@users.noreply.github.com>
2022-02-28 10:21:51 +0530

    Add files via upload

---
+++ phase1/assisted/AccessSpeci.java
@@ -0,0 +1,97 @@
+package AssistedPracProj;
+
+class defAccessSpecifier
+{ 
+  void display() 
+     { 
+         System.out.println("You are using defalut access specifier"); 
+     } 
+} 
+
+ class accessSpecifiers1 extends defAccessSpecifier {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Dafault Access Specifier");
+		defAccessSpecifier obj = new defAccessSpecifier(); 		  
+        obj.display(); 
+
+	}
+}
+
+
+
+class priaccessspecifier 
+{ 
+} 
+
+ class accessSpecifiers2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Private Access Specifier");
+		priaccessspecifier  obj = new priaccessspecifier(); 
+         
+        //obj.display();
+
+	}
+}
+
+
+
+
+
+ class proaccessspecifiers {
+
+	protected void display() 
+    { 
+        System.out.println("This is protected access specifier"); 
+    } 
+}
+
+
+
+
+class accessSpecifiers3 extends proaccessspecifiers {
+
+	public static void main(String[] args) {
+		accessSpecifiers3 obj = new accessSpecifiers3 ();   
+	       obj.display();  
+	}
+
+}
+
+
+
+
+
+
+
+
+
+class pubaccessspecifiers {
+
+	public void display() 
+    { 
+        System.out.println("This is Public Access Specifiers"); 
+        
+    } 
+}
+
+
+
+public class AccessSpeci  {
+
+	public static void main(String[] args) {
+		
+		pubaccessspecifiers obj = new pubaccessspecifiers();
+		accessSpecifiers3 a=new accessSpecifiers3();
+		accessSpecifiers1 b= new accessSpecifiers1();
+        obj.display();  
+        a.display();
+        b.display();
+		
+	}
+}
+
+
