b9893f081455dd9e74553a448ce6c4565af88706
kartheek473 <100348749+kartheek473@users.noreply.github.com>
2022-03-01 16:23:41 +0530

    Add files via upload

---
+++ phase1/assisted/Constructors.java
@@ -0,0 +1,37 @@
+package AssistedPracProj;
+
+public class Constructors {
+
+	public static void main(String[] args) {
+
+		EmpInfo emp1 = new EmpInfo();
+		emp1.display();
+
+		Para std1 = new Para(2, "Abcd");
+		std1.display();
+
+	}
+}
+
+class EmpInfo {
+	int id = 10;
+	String name;
+
+	void display() {
+		System.out.println(id + " " + name);
+	}
+}
+
+class Para {
+	int id;
+	String name;
+
+	Para(int i, String n) {
+		id = i;
+		name = n;
+	}
+
+	void display() {
+		System.out.println(id + " " + name);
+	}
+}
